8f170966b41f491c2b68b3dc8314a00f8ca24ea3
Jack Corddry <jackcorddry@gmail.com>
2025-02-20 14:57:56 -0800

    Throw spaghetti at wall

---
+++ src/EscrowActionVerifier.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+
+
+/// @title EscrowActionVerifier
+/// @notice 
+contract EscrowActionVerifier {
+
+    struct ActionParams {
+        bytes32 marketHash; // Implies action that will be rewarded
+        address LP;
+        address token;
+        uint256 amount;
+    }
+
+    // The address of the escrow
+    address public escrow;
+
+    mapping(bytes32 => mapping(address => bool)) public claimed;
+    
+    constructor(address _escrow) {
+        escrow = _escrow;
+    }
+
+    function getRewards()
+
+    // Add your custom logic here
+    function processClaim() {
+        
+    }
+        bytes memory /* action */
+        bytes[] memory /* signatures */
+    ) external pure returns (bool) {
+        // TODO: Implement verification logic
+        return false;
+    }
+}
