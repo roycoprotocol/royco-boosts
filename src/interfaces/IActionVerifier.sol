7d59fc7b16e78b2aaacbc7e44e5eb056c28b48d8
Shivaansh Kapoor <shivaanshkapoor02@gmail.com>
2025-03-26 16:15:20 -0700

    fmt

--- src/interfaces/IActionVerifier.sol
+++ src/interfaces/IActionVerifier.sol
@@ -1,62 +1,58 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-/**
- * @title IActionVerifier
- * @notice Interface for processing and verifying actions related to incentive campaigns.
- */
+/// @title IActionVerifier
+/// @notice ActionVerifier interface for processing incentive campaign creation, modifications, and payouts.
 interface IActionVerifier {
-    /**
-     * @notice Processes incentive campaign creation by validating the provided parameters.
-     * @param _incentiveCampaignId A unique hash identifier for the incentive campaign in the incentive locker.
-     * @param _actionParams Arbitrary parameters defining the action.
-     * @param _ip The address placing the incentives for this campaign.
-     * @return valid Returns true if the incentive campaign creation is valid.
-     */
-    function processIncentiveCampaignCreation(bytes32 _incentiveCampaignId, bytes memory _actionParams, address _ip)
-        external
-        returns (bool valid);
+    /// @notice Processes incentive campaign creation by validating the provided parameters.
+    /// @param _incentiveCampaignId A unique hash identifier for the incentive campaign in the incentive locker.
+    /// @param _actionParams Arbitrary parameters defining the action.
+    /// @param _ip The address placing the incentives for this campaign.
+    /// @return valid Returns true if the incentive campaign creation is valid.
+    function processIncentiveCampaignCreation(bytes32 _incentiveCampaignId, bytes memory _actionParams, address _ip) external returns (bool valid);
 
-    /**
-     * @notice Processes the addition of incentives for a given campaign.
-     * @param _incentiveCampaignId The unique identifier for the incentive campaign.
-     * @param _incentivesOffered The list of incentive token addresses offered in the campaign.
-     * @param _incentiveAmountsOffered The corresponding amounts offered for each incentive token.
-     * @param _ip The address placing the incentives for this campaign.
-     * @return valid Returns true if the incentives were successfully added.
-     */
+    /// @notice Processes the addition of incentives for a given campaign.
+    /// @param _incentiveCampaignId The unique identifier for the incentive campaign.
+    /// @param _incentivesOffered The list of incentive token addresses offered in the campaign.
+    /// @param _incentiveAmountsOffered The corresponding amounts offered for each incentive token.
+    /// @param _ip The address placing the incentives for this campaign.
+    /// @return valid Returns true if the incentives were successfully added.
     function processIncentivesAdded(
         bytes32 _incentiveCampaignId,
         address[] memory _incentivesOffered,
         uint256[] memory _incentiveAmountsOffered,
         address _ip
-    ) external returns (bool valid);
+    )
+        external
+        returns (bool valid);
 
-    /**
-     * @notice Processes the removal of incentives for a given campaign.
-     * @param _incentiveCampaignId The unique identifier for the incentive campaign.
-     * @param _incentivesToRemove The list of incentive token addresses to be removed from the campaign.
-     * @param _incentiveAmountsToRemove The corresponding amounts to remove for each incentive token.
-     * @param _ip The address placing the incentives for this campaign.
-     * @return valid Returns true if the incentives were successfully removed.
-     */
+    /// @notice Processes the removal of incentives for a given campaign.
+    /// @param _incentiveCampaignId The unique identifier for the incentive campaign.
+    /// @param _incentivesToRemove The list of incentive token addresses to be removed from the campaign.
+    /// @param _incentiveAmountsToRemove The corresponding amounts to remove for each incentive token.
+    /// @param _ip The address placing the incentives for this campaign.
+    /// @return valid Returns true if the incentives were successfully removed.
     function processIncentivesRemoved(
         bytes32 _incentiveCampaignId,
         address[] memory _incentivesToRemove,
         uint256[] memory _incentiveAmountsToRemove,
         address _ip
-    ) external returns (bool valid);
+    )
+        external
+        returns (bool valid);
 
-    /**
-     * @notice Processes a claim by validating the provided parameters.
-     * @param _ap The address of the action provider (AP) making the claim.
-     * @param _incentiveCampaignId The unique identifier for the incentive campaign used for the claim.
-     * @param _claimParams Encoded parameters required for processing the claim.
-     * @return valid Returns true if the claim is valid.
-     * @return incentives The incentive token addresses to be paid out to the AP.
-     * @return incentiveAmountsOwed The amounts owed for each incentive token in the incentives array.
-     */
-    function processClaim(address _ap, bytes32 _incentiveCampaignId, bytes memory _claimParams)
+    /// @notice Processes a claim by validating the provided parameters.
+    /// @param _ap The address of the action provider (AP) making the claim.
+    /// @param _incentiveCampaignId The unique identifier for the incentive campaign used for the claim.
+    /// @param _claimParams Encoded parameters required for processing the claim.
+    /// @return valid Returns true if the claim is valid.
+    /// @return incentives The incentive token addresses to be paid out to the AP.
+    /// @return incentiveAmountsOwed The amounts owed for each incentive token in the incentives array.
+    function processClaim(
+        address _ap,
+        bytes32 _incentiveCampaignId,
+        bytes memory _claimParams
+    )
         external
         returns (bool valid, address[] memory incentives, uint256[] memory incentiveAmountsOwed);
 }
